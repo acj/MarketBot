d2968f57252f0162ccf92466bcc63013413970ab
Adam Jensen <acj@linuxguy.org>
2014-03-29 16:45:56 -0400

    Add GooglePlayWatcher class

---
+++ src/org/linuxguy/MarketBot/GooglePlayWatcher.java
@@ -0,0 +1,55 @@
+package org.linuxguy.MarketBot;
+
+import com.gc.android.market.api.MarketSession;
+import com.gc.android.market.api.model.Market;
+
+import java.util.concurrent.TimeUnit;
+
+public class GooglePlayWatcher extends Watcher<Comment> implements MarketSession.Callback<Market.CommentsResponse> {
+    private static final long POLL_INTERVAL_MS = TimeUnit.MINUTES.toMillis(5);
+
+    private String mUsername;
+    private String mPassword;
+    private String mAppName;
+    private String mAppId;
+
+    public GooglePlayWatcher(String username, String password, String appName, String appId) {
+        mUsername = username;
+        mPassword = password;
+        mAppName = appName;
+        mAppId = appId;
+    }
+
+    @Override
+    public void run() {
+        MarketSession session = new MarketSession();
+        session.login(mUsername, mPassword);
+
+        while (true) {
+            Market.CommentsRequest commentsRequest = Market.CommentsRequest.newBuilder()
+                    .setAppId(mAppId)
+                    .setStartIndex(0)
+                    .setEntriesCount(5)
+                    .build();
+
+            session.append(commentsRequest, this);
+
+            session.flush();
+
+            try {
+                Thread.sleep(POLL_INTERVAL_MS);
+            } catch (InterruptedException e) {
+                break;
+            }
+        }
+    }
+
+    @Override
+    public void onResult(Market.ResponseContext responseContext, Market.CommentsResponse response) {
+        for (Market.Comment c : response.getCommentsList()) {
+            if (c.getCreationTime() > mLastPollTime) {
+                notifyListeners(Comment.from(c));
+            }
+        }
+    }
+}
