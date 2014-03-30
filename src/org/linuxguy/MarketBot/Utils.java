f2770bfc3b29de38cd690a8cdc53994b19c593da
Adam Jensen <acj@linuxguy.org>
2014-03-29 22:11:18 -0400

    Add Utils#fetch{String,Json}FromUrl methods (and Jackson libs)

--- src/org/linuxguy/MarketBot/Utils.java
+++ src/org/linuxguy/MarketBot/Utils.java
@@ -1,6 +1,14 @@
 package org.linuxguy.MarketBot;
 
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.io.UnsupportedEncodingException;
+import java.net.URL;
+import java.net.URLConnection;
 
 public class Utils {
 
@@ -24,6 +32,44 @@ public class Utils {
                 return "★★★★★";
             default:
                 return "";
+        }
+    }
+
+    public static JsonNode fetchJsonFromUrl(String url) {
+        String text = fetchStringFromUrl(url);
+
+        ObjectMapper objectMapper = new ObjectMapper();
+
+        try {
+            return objectMapper.readTree(text);
+        } catch (IOException e) {
+            e.printStackTrace();
+
+            return null;
+        }
+    }
+
+    public static String fetchStringFromUrl(String urlToFetch) {
+        try {
+            URL url = new URL(urlToFetch);
+            URLConnection connection = null;
+            connection = url.openConnection();
+            BufferedReader in = new BufferedReader(new InputStreamReader(connection.getInputStream()));
+
+            StringBuilder response = new StringBuilder();
+            String inputLine;
+
+            while ((inputLine = in.readLine()) != null) {
+                response.append(inputLine);
+            }
+
+            in.close();
+
+            return response.toString();
+        } catch (IOException e) {
+            e.printStackTrace();
+
+            return "";
         }
     }
 }
