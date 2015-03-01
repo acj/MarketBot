e249cb25211dd5ab844568b93afafe67a47983ca
Adam Jensen <acj@linuxguy.org>
2015-02-28 20:08:18 -0500

    Implement Comment#equals

--- src/main/java/org/linuxguy/MarketBot/Comment.java
+++ src/main/java/org/linuxguy/MarketBot/Comment.java
@@ -5,4 +5,26 @@ public class Comment {
     public int    rating;
     public String text;
     public long   timestamp;
+
+    @Override
+    public boolean equals(Object that) {
+        if (that instanceof Comment) {
+            Comment thatComment = (Comment)that;
+
+            return safeStringEquals(this.author, thatComment.author) &&
+                    this.rating == thatComment.rating &&
+                    safeStringEquals(this.text, thatComment.text) &&
+                    this.timestamp == thatComment.timestamp;
+        }
+
+        return false;
+    }
+
+    private static boolean safeStringEquals(String s1, String s2) {
+        if (s1 == null) {
+            return s2 == null;
+        } else {
+            return s1.equals(s2);
+        }
+    }
 }
