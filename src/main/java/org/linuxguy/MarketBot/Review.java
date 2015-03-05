97aa5e2e6577c81e071f64785b182e9ac725ce05
Adam Jensen <acj@linuxguy.org>
2015-03-04 20:59:10 -0500

    Add title and deviceName fields to Review

--- src/main/java/org/linuxguy/MarketBot/Review.java
+++ src/main/java/org/linuxguy/MarketBot/Review.java
@@ -3,20 +3,26 @@ package org.linuxguy.MarketBot;
 public class Review {
     public String author;
     public int    rating;
+    public String title;
     public String text;
     public long   timestamp;
     public String version;
+    public String deviceName;
     public String countryCode;
 
     @Override
     public boolean equals(Object that) {
         if (that instanceof Review) {
-            Review thatComment = (Review)that;
+            Review thatReview = (Review)that;
 
-            return safeStringEquals(this.author, thatComment.author) &&
-                    this.rating == thatComment.rating &&
-                    safeStringEquals(this.text, thatComment.text) &&
-                    this.timestamp == thatComment.timestamp;
+            return safeStringEquals(this.author, thatReview.author) &&
+                    this.rating == thatReview.rating &&
+                    safeStringEquals(this.title, thatReview.title) &&
+                    safeStringEquals(this.text, thatReview.text) &&
+                    this.timestamp == thatReview.timestamp &&
+                    safeStringEquals(this.version, thatReview.version) &&
+                    safeStringEquals(this.deviceName, thatReview.deviceName) &&
+                    safeStringEquals(this.countryCode, thatReview.countryCode);
         }
 
         return false;
